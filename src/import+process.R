12c7931de229acc0c4a42036b71ebf35f3855a52
DitteM <ditte.lonborg@gmail.com>
2022-11-13 18:51:03 +0100

    Data process updated

--- src/import+process.R
+++ src/import+process.R
@@ -8,18 +8,18 @@ library(tidyverse)
 # use fill from tidyr to fill enhed column. 
 # --> fills values top-down until it encounters next valid value
 
-df <- read_xlsx("all_data.xlsx", skip = 2) %>% 
-  rename(enhed=`...1`,varegruppe=`...2`) %>% 
+df <- read_xlsx("../data/all_data.xlsx", skip = 2) %>% 
+ rename(enhed=`...1`,varegruppe=`...2`) %>% 
   fill(enhed, .direction="down")
 
 # convert all except first to columns to numeric 
 
 df[, 3:ncol(df)] <- df[, 3:ncol(df)] %>% 
   mutate_if(is.character, as.numeric)
-  
+
 # check that was before ".." is now NA (i.e., no inconsistencies in formatting)
 
-temp <- read_xlsx("all_data.xlsx", skip = 2) %>% 
+temp <- read_xlsx("../data/all_data.xlsx", skip = 2) %>% 
   rename(enhed=`...1`,varegruppe=`...2`) %>% 
   fill(enhed, .direction="down")
 
@@ -31,3 +31,40 @@ sum(temp == "..") == sum(is.na(df))
 # big data frame
 
 df <- df %>% pivot_longer(cols=`2020M01`:`2022M08`,names_to = "maaned",values_to="vaerdi")
+
+# instead of having three rows for one group with values (indeks, ...), 
+# have one column per group with respective values
+# split code and description afterwards
+# rename columns
+
+df <- df %>% 
+  group_by(enhed) %>%
+  pivot_wider(names_from = enhed, values_from = vaerdi) %>% 
+  separate(varegruppe, into=c("kode","beskrivelse"),sep="\\s",extra="merge") %>% 
+  rename(vaerdi_i = Indeks, vaerdi_ae_m = `Ændring i forhold til måneden før (pct.)`, vaerdi_ae_aa = `Ændring i forhold til samme måned året før (pct.)`)
+
+# create code levels
+
+df <- df %>%
+  mutate(niv_1 = ifelse(str_count(kode,"\\.")==0 | str_count(kode,"\\.")==1 & nchar(kode)==3,kode,NA),
+         niv_2=ifelse(str_count(kode,"\\.")==1 & nchar(kode) > 3,kode,NA),
+         niv_3=ifelse(str_count(kode,"\\.")==2,kode,NA),
+         niv_4=ifelse(str_count(kode,"\\.")==3,kode,NA))
+
+# making sure every category is on same form
+df <- df %>%
+  mutate(niv_1 = ifelse(niv_1 == "00", "00.", niv_1),
+         niv_1 = ifelse(niv_1 == "01", "01.", niv_1),
+         niv_4 = ifelse(str_count(niv_4)==8, niv_4, substr(niv_4, star=1, stop = 8)))
+
+# adding trace of levels to the data
+df <- df %>%
+  mutate(niv_3 = ifelse(is.na(niv_3), substr(niv_4, start=1, stop = 6), niv_3),
+         niv_2 = ifelse(is.na(niv_2), substr(niv_3, start=1, stop = 4), niv_2),
+         niv_1 = ifelse(is.na(niv_1), substr(niv_2, start=1, stop = 3), niv_1))
+
+# Adding column with date converted to type = Date
+df <- df %>%
+  mutate(Dato = str_replace(maaned, "M", "-")) %>%
+  mutate(Dato = paste0(as.character(Dato), "-01")) %>% 
+  mutate(Dato = as.Date(Dato, format = "%Y-%m-%d"))
